d900b65a9b183d564010407ff9046bd3c6d013b4
Sam Holmes <samholmesdev@gmail.com>
2017-07-27 09:44:23 +0100

    Setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='python-to-json',
+    version='1.0.0',
+    author='Ingresso',
+    author_email='systems@ingresso.co.uk',
+    url='https://github.com/ingresso-group/python-to-json/',
+    license='MIT',
+    description='Module for serializing python objects to json',
+    install_requires=[],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ],
+)
